aaf44278f4ea61ab60452dd5706328b495c2a5d5
Eric Chung <cinchurge@gmail.com>
2014-12-08 16:54:08 -0800

    In the process of reworking 3.47

--- exercises/03_47.scm
+++ exercises/03_47.scm
@@ -1,19 +1,38 @@
+(use extras)
+
+(define (make-mutex)
+  (let ((cell (list #f)))            
+    (define (the-mutex m)
+      (cond ((eq? m 'acquire)
+             (if (test-and-set! cell)
+                 (the-mutex 'acquire))) ; retry
+            ((eq? m 'release) (clear! cell))))
+    the-mutex))
+(define (clear! cell)
+  (set-car! cell #f))
+
+(define (test-and-set! cell)
+  (if (car cell)
+      #t
+      (begin (set-car! cell #t)
+             #f)))
 
 (define (make-sem n)
-    (let ((mutex (make-mutex))
-          (n 0))
-        (lambda (the-sem m)
-            (cond ((eq? m 'acquire)
-                   (begin
-                       (mutex 'acquire)
+    (let ((mutex (make-mutex)) (count 0))
+        (define (the-sem m)
+            (mutex 'acquire)
+            (cond ((eq? m 'acquire) (set! count (+ count 1)))
+                  ((eq? m 'release) (set! count (- count 1))))
+            (mutex 'release)
+            (printf "count=~S~N" count))
+        the-sem))
 
 
 
 
 
 (let ((sem (make-sem 3)))
-    (begin
-        (sem 'acquire)
-        (sem 'acquire)
-        (sem 'acquire)
-        (sem 'acquire)))
+    (sem 'acquire)
+    (sem 'acquire)
+    (sem 'acquire)
+    (sem 'acquire))
