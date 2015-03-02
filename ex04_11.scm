d3e02ed316229ee5001162f83235bbbc9aecdc82
Eric Chung <cinchurge@gmail.com>
2015-03-02 14:27:26 -0800

    Completed exercise 4.11

--- ex04_11.scm
+++ ex04_11.scm
@@ -2,17 +2,34 @@
 (use extras)
 (load "metacircular.scm")
 
+; In order to use set-car! and set-cdr!, the
+; frame must looks something like this:
+; (() (pair1 pair2 pair3 ...))
 (define (make-frame vars vals)
-  (if (or (null? vars) (null? vals))
-      '()
-      (cons (cons (car vars) (car vals)) (make-frame (cdr vars) (cdr vals)))))
+  (define (make-frame-iter vars vals)
+    (if (null? vars)
+        '()
+        (cons (cons (car vars) (car vals)) (make-frame-iter (cdr vars) (cdr vals)))))
+  (cons '() (make-frame-iter vars vals)))
+
+
+
+; We use (frame-pairs frame) to extract the list of pairs
+(define (frame-pairs frame)
+  (cdr frame))
+
+
+
+; Add a pair to the head of the list of pairs
+(define (add-binding-to-frame! var val frame)
+  (set-cdr! frame (cons (cons var val) (frame-pairs frame))))
 
 
 
 (define (set-variable-value! var val env)
   (define (env-loop env)
     (define (scan frame)
-      (cond ((assoc var frame) => (lambda (x) (set-cdr! x val)))
+      (cond ((assoc var (frame-pairs frame)) => (lambda (x) (set-cdr! x val)))
             (else (enclosing-environment env))))
     (if (eq? env the-empty-environment)
         (error "Unbound variable: SET!" var)
@@ -25,7 +42,7 @@
 (define (lookup-variable-value var env)
   (define (env-loop env)
     (define (scan frame)
-      (cond ((assoc var frame) => cdr)
+      (cond ((assoc var (frame-pairs frame)) => cdr)
             (else (env-loop (enclosing-environment env)))))
     (if (eq? env the-empty-environment)
         (error "Unbound variable" var)
@@ -35,35 +52,41 @@
 
 
 
-(define (add-binding-to-frame! var val frame)
-  (let ((new-frame (cons (cons var val) (copy frame))))
-    (printf "~S~N" new-frame)
-    (set-car! frame new-frame))
-    (printf "~S~N" frame))
-
-
-
 (define (define-variable! var val env)
   (let ((frame (first-frame env)))
     (define (scan frame)
-      (cond ((assoc var frame) => (lambda (x) (set-cdr! x val)))
+      (cond ((assoc var (frame-pairs frame)) => (lambda (x) (set-cdr! x val)))
             (else (add-binding-to-frame! var val frame))))
     (scan frame)))
 
 
 
+; Unit tests
+; Make sure the evaluator still works
+(printf "Metacircular evaluator unit tests~N")
+(load "test-metacircular.scm")
+(printf "~N")
+
+(printf "Exercise 4.11 unit tests~N")
+; Test variable binding and lookup
 (let ((env (setup-environment)))
-  (test '((a . 1) (b . 2) (c . 3)) (make-frame '(a b c) '(1 2 3)))
+  (test '(() (a . 1) (b . 2) (c . 3)) (make-frame '(a b c) '(1 2 3)))
+  (let ((env2 (extend-environment '() '() env)))
+    (define-variable! 'a 0 env2)
+    (test 0 (lookup-variable-value 'a env2))
+    (define-variable! 'a 1 env2)
+    (test 1 (lookup-variable-value 'a env2))
+    (define-variable! 'd 4 env2)
+    (test 4 (lookup-variable-value 'd env2))
+  )
   (let ((env2 (extend-environment '(a b c) '(1 2 3) env)))
     (set-variable-value! 'a 0 env2)
     (test 0 (lookup-variable-value 'a env2))
     (define-variable! 'a 1 env2)
     (test 1 (lookup-variable-value 'a env2))
     (define-variable! 'd 4 env2)
-    (printf "~S~N" env2)
     (test 4 (lookup-variable-value 'd env2))
   )
 )
+(printf "~N")
 
-
-(load "test-metacircular.scm")
