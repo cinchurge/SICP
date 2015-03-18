218508a38aa265d69369160a539b806d2f5e4b5f
Eric <cinchurge@gmail.com>
2015-03-18 08:43:54 -0700

    Updates to 2.1.4 problems

--- 02_01_04.scm
+++ 02_01_04.scm
@@ -15,3 +15,33 @@
    x
    (make-interval (/ 1.0 (upper-bound x))
                   (/ 1.0 (lower-bound y)))))
+
+; Exercise 2.7:
+; Implement the constructor and selectors
+(define (make-interval a b) (cons a b))
+(define (upper-bound x) (cdr x))
+(define (lower-bound x) (car x))
+
+; Exercise 2.8:
+; Describe how the difference of two intervals
+; may be computed and the corresponsding procedure
+; sub-interval
+
+; The difference d = b - a between two intervals a & b calculated
+; should be defined so that a + d = b
+(define (sub-interval a b)
+  (make-interval (- (lower-bound x) (lower-bound y))
+                 (- (upper-bound x) (upper-bound y))))
+
+; Exercise 2.9:
+
+; Exercise 2.10:
+; Modify div-interval so that we don't divide by zero
+(define (div-interval x y)
+  (if (or (= upper-bound 0) (= lower-bound 0))
+      (error "Division by zero: DIV-INTERVAL")
+      (mul-interval
+       x
+       (make-interval (/ 1.0 (upper-bound x))
+                      (/ 1.0 (lower-bound y))))))
+
